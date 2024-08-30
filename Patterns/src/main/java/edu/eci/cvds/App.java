3de1bfef9a2d8e0ca152676e28a093772c625dc6
thesrcielos <diegomadi75@gmail.com>
2024-08-29 22:16:08 -0500

    corrigiendo detalles en la parte de exec

--- Patterns/src/main/java/edu/eci/cvds/App.java
+++ Patterns/src/main/java/edu/eci/cvds/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.printf( "Hello %s %s!", args[0], args[1]);
+        if(args == null || args.length==0){
+            System.out.println("Hello world!");
+        }else {
+            System.out.printf("Hello %s %s!", args[0], args[1]);
+        }
     }
 }
